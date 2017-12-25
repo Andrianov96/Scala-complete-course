48bc90807818978bc1f95ea6d5d091d9ae0c378e
g.andrianov <g.andrianov@tinkoff.ru>
2017-12-25 11:55:30 +0300

    use ordering

--- src/main/scala/lectures/oop/types/GeneralBST.scala
+++ src/main/scala/lectures/oop/types/GeneralBST.scala
@@ -15,6 +15,8 @@ import lectures.matching.SortingStuff.Watches
   * * * * Watches из задачи SortStuff. Большими считаются часы с большей стоимостью
   */
 
+
+
 trait GeneralBST[T] {
   val value: T
   val left: Option[GeneralBST[T]]
@@ -25,51 +27,38 @@ trait GeneralBST[T] {
   def find(value: T): Option[GeneralBST[T]]
 }
 
-trait TypeForBST[T] {
-  def isGreater(f: T, s: T): Boolean
-  def isLess(f: T, s: T): Boolean
+trait TypeForBST[T] extends Ordering[T]{
+  def compare(f: T, s: T): Int
 }
 
-class GeneralBSTImpl[T: TypeForBST](val value: T, val left: Option[GeneralBST[T]] = None, val right: Option[GeneralBST[T]] = None) extends GeneralBST[T] {
-  val r = implicitly[TypeForBST[T]]
 
-  override def find(valueToFind: T): Option[GeneralBST[T]] = {
-    if (r.isLess(valueToFind, value)) this.left.map(_.find(valueToFind)).getOrElse(None)
-    else if (r.isGreater(valueToFind, value)) this.right.map(_.find(valueToFind)).getOrElse(None)
+
+class GeneralBSTImpl[T: TypeForBST](val value: T, val left: Option[GeneralBST[T]] = None, val right: Option[GeneralBST[T]] = None) extends GeneralBST[T] {
+  val comparator = implicitly[TypeForBST[T]]
+
+  override def find(valueToFind: T) : Option[GeneralBST[T]] = {
+    if (comparator.compare(valueToFind, value) < 0) this.left.map(_.find(valueToFind)).getOrElse(None)
+    else if (comparator.compare(valueToFind, value) > 0) this.right.map(_.find(valueToFind)).getOrElse(None)
     else Some(this)
   }
 
   override def add(newValue: T): GeneralBST[T] = {
-    if (r.isGreater(newValue, value)) new GeneralBSTImpl(value, left, Some(right.map(_.add(newValue)).getOrElse(new GeneralBSTImpl[T](newValue))))
-      else if (r.isLess(newValue, value)) new GeneralBSTImpl(value, Some(left.map(_.add(newValue)).getOrElse(new GeneralBSTImpl[T](newValue))), right)
+    if (comparator.compare(newValue, value) < 0) new GeneralBSTImpl(value, left, Some(right.map(_.add(newValue)).getOrElse(new GeneralBSTImpl[T](newValue))))
+      else if (comparator.compare(newValue, value) > 0) new GeneralBSTImpl(value, Some(left.map(_.add(newValue)).getOrElse(new GeneralBSTImpl[T](newValue))), right)
         else this
   }
 }
 
 object ImplicitsDeclaration extends App{
   implicit val float2BST: TypeForBST[Float] = new TypeForBST[Float] {
-    override def isGreater(f: Float, s: Float): Boolean = f > s
-
-    override def isLess(f: Float, s: Float): Boolean = f < s
+    def compare(f: Float, s: Float) = f compare s
   }
 
   implicit val string2BST: TypeForBST[String] = new TypeForBST[String] {
-    override def isGreater(f: String, s: String): Boolean = f > s
-
-    override def isLess(f: String, s: String): Boolean = f < s
+    def compare(f: String, s: String) = f compare s
   }
 
   implicit val watches2BST: TypeForBST[Watches] = new TypeForBST[Watches] {
-    override def isGreater(f: Watches, s: Watches): Boolean = (f, s) match {
-      case (a, b) if a.brand > b.brand => true
-      case (a, b) if a.brand == b.brand && a.cost > b.cost => true
-      case _ => false
-    }
-
-    override def isLess(f: Watches, s: Watches): Boolean = (f, s) match {
-      case (a, b) if a.brand < b.brand => true
-      case (a, b) if a.brand == b.brand && a.cost < b.cost => true
-      case _ => false
-    }
+    def compare(f: Watches, s: Watches) = f.cost compare s.cost
   }
 }

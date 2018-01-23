decb22a7fa0266feb02e56600747253c74ab1c0f
g.andrianov <g.andrianov@tinkoff.ru>
2018-01-23 13:47:21 +0300

    version 1.0

--- src/main/scala/lectures/oop/FatUglyController.scala
+++ src/main/scala/lectures/oop/FatUglyController.scala
@@ -29,35 +29,30 @@ case class File(name: String, body: String, extension: String){
   def this(file: String) = this(file.trim.takeWhile(_ != '\n'), file.trim.dropWhile( _ != '\n').drop(1), file.trim.takeWhile(_ != '\n').dropWhile(_ != '.').drop(1))
 }
 
-class FatUglyController {
-
-  val MaxFileSize = 8388608
+class FatUglyController(val MaxFileByteSize: Int = 8388608) {
 
   def processRoute(route: String, requestBody: Option[Array[Byte]]): (Int, String) =
     (route, requestBody) match {
       case (rout, _) if rout != "/api/v1/uploadFile" => (404, "Route not found")
       case (_, reqBody) if reqBody.isEmpty => (400, "Can not upload empty file")
-      case (_, reqBody) if reqBody.get.length > MaxFileSize => (400, s"File size should not be more than ${MaxFileSize / (1024 *1024)} MB")
+      case (_, reqBody) if reqBody.get.length > MaxFileByteSize => (400, s"File size should not be more than ${MaxFileByteSize / (1024 *1024)} MB")
       case _ => {
-        val responseBuf = new StringBuilder()
-        val databaseConnectionId = connectToPostgresDatabase()
-        val mqConnectionId = connectToIbmMq()
-        initializeLocalMailer()
         val files = getFiles(requestBody)
         if (hasBadExtention(files))
           (400, "Request contains forbidden extension")
         else {
+          val responseBuf = new StringBuilder()
+          initializeLocalMailer()
           files.foreach { file =>
             val id = hash(file)
             // Emulate file saving to disk
-            responseBuf.append(s"- saved file ${file.name} to " + id + "." + file.extension + s" (file size: ${file.body.length})" + "\n")
+            responseBuf.append(s"- saved file ${file.name} to $id.${file.extension} (file size: ${file.body.length})\n")
 
-            executePostgresQuery(databaseConnectionId, s"insert into files (id, name, created_on) values ('$id', '${file.name}', current_timestamp)")
-            sendMessageToIbmMq(mqConnectionId, s"""<Event name="FileUpload"><Origin>SCALA_FTK_TASK</Origin><FileName>${file.name}</FileName></Event>""")
-            send("admin@admin.tinkoff.ru", "File has been uploaded", s"Hey, we have got new file: ${file.name}")
+            writeFileToDB(id, file.name)
+            sendMsgToIbmMq(file.name)
+            emailToAdminAboutNewFile(file.name)
           }
-
-          (200, "Response:\r\n" + responseBuf.dropRight(1))
+          (200, "Response:\n" + responseBuf.dropRight(1))
         }
       }
     }
@@ -81,6 +76,18 @@ class FatUglyController {
     13 // chosen by fair dice roll
   }
 
+  def writeFileToDB(id: String, fileName: String): String = {
+    val connectionId = connectToPostgresDatabase()
+    executePostgresQuery(connectionId, s"insert into files (id, name, created_on) values ('$id', '$fileName', current_timestamp)")
+  }
+
+
+  def sendMsgToIbmMq(fileName:String): String = {
+    val connectionId = connectToIbmMq()
+    sendMessageToIbmMq(connectionId, s"""<Event name="FileUpload"><Origin>SCALA_FTK_TASK</Origin><FileName>${fileName}</FileName></Event>""")
+  }
+
+
   def sendMessageToIbmMq(connectionId: Int, message: String): String = {
     // DO NOT TOUCH
     println(s"Sent MQ message via $connectionId: $message")
@@ -90,6 +97,11 @@ class FatUglyController {
   def initializeLocalMailer(): Unit = {
     // DO NOT TOUCH
     println("Initialized local mailer")
+  }
+
+  def emailToAdminAboutNewFile(fileName: String): String = {
+    send("admin@admin.tinkoff.ru", "File has been uploaded", s"Hey, we have got new file: $fileName")
+    s"""Send email to admin@admin.tinkoff.ru: Theme "File has been uploaded" body "Hey, we have got new file: $fileName""""
   }
 
   def send(email: String, subject: String, body: String): Unit = {
--- src/test/scala/lectures/oop/FatUglyControllerTest.scala
+++ src/test/scala/lectures/oop/FatUglyControllerTest.scala
@@ -2,6 +2,8 @@ package lectures.oop
 
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.util.Random
+
 class FatUglyControllerTest extends FlatSpec with Matchers {
 
   behavior of "FatUglyController"
@@ -15,9 +17,7 @@ class FatUglyControllerTest extends FlatSpec with Matchers {
     val (status, body) = controller.processRoute("/api/v1/uploadFile", Some(requestBody.getBytes))
 
     status shouldBe 200
-    body shouldBe
-      """Response:
-        |- saved file file1.txt to 063f83f94e59aac2edd719fab1d179f86084887a.txt (file size: 21)"""
+    body shouldBe "Response:\n- saved file file1.txt to 063f83f94e59aac2edd719fab1d179f86084887a.txt (file size: 21)"
         .stripMargin
   }
 
@@ -33,10 +33,7 @@ class FatUglyControllerTest extends FlatSpec with Matchers {
     val (status, body) = controller.processRoute("/api/v1/uploadFile", Some(requestBody.getBytes))
 
     status shouldBe 200
-    body shouldBe
-      """Response:
-        |- saved file file1.txt to 063f83f94e59aac2edd719fab1d179f86084887a.txt (file size: 21)
-        |- saved file file2.txt to 7387fa41a69d93b59b67bd46ab18a72c81edb767.txt (file size: 23)"""
+    body shouldBe "Response:\n- saved file file1.txt to 063f83f94e59aac2edd719fab1d179f86084887a.txt (file size: 21)\n- saved file file2.txt to 7387fa41a69d93b59b67bd46ab18a72c81edb767.txt (file size: 23)"
         .stripMargin
   }
 
@@ -54,12 +51,41 @@ class FatUglyControllerTest extends FlatSpec with Matchers {
     body shouldBe "Can not upload empty file"
   }
 
-  ignore should "return 400 for forbidden extension" in {
+  it should "return 400 for forbidden extension" in {
+    val requestBody =
+      """DELIMITER
+        |file1.exe
+        |This is body of file1
+      """.stripMargin
+    val (status, body) = controller.processRoute("/api/v1/uploadFile", Some(requestBody.getBytes))
 
+    status shouldBe 400
+    body shouldBe "Request contains forbidden extension"
   }
 
-  ignore should "return 400 for file greater than 8 MB" in {
+  it should "return 400 for file greater than 8 MB" in {
+    var testFile = "a" * (1024 * 10250)
+    val requestBody = "DELIMITER\r\nfile1.txt\r\n" + testFile
+    val (status, body) = controller.processRoute("/api/v1/uploadFile", Some(requestBody.getBytes))
 
+    status shouldBe 400
+    body shouldBe s"File size should not be more than ${controller.MaxFileByteSize / (1024 *1024)} MB"
+  }
+
+  "writeToDB" should "write to DB" in {
+    val id = Random.nextString(10)
+    val fileName = Random.nextString(10)
+    controller.writeFileToDB(id, fileName) shouldBe s"Result of insert into files (id, name, created_on) values ('$id', '$fileName', current_timestamp)"
+  }
+
+  "sendMsgToIbmMq" should "send message to IbmMq" in {
+    val fileName = Random.nextString(10)
+    controller.sendMsgToIbmMq(fileName) shouldBe s"""Message sending result for <Event name="FileUpload"><Origin>SCALA_FTK_TASK</Origin><FileName>${fileName}</FileName></Event>"""
+  }
+
+  "send" should "send email" in {
+    val fileName = Random.nextString(10)
+    controller.emailToAdminAboutNewFile(fileName) shouldBe s"""Send email to admin@admin.tinkoff.ru: Theme "File has been uploaded" body "Hey, we have got new file: $fileName""""
   }
 
   private val controller = new FatUglyController()

3e8d2174cd62b09fdfb88bb4833013bd988f5ff8
g.andrianov <g.andrianov@tinkoff.ru>
2018-02-26 16:16:21 +0300

    Fix GeneralBST

--- src/main/scala/lectures/oop/types/GeneralBST.scala
+++ src/main/scala/lectures/oop/types/GeneralBST.scala
@@ -27,38 +27,23 @@ trait GeneralBST[T] {
   def find(value: T): Option[GeneralBST[T]]
 }
 
-trait TypeForBST[T] extends Ordering[T]{
-  def compare(f: T, s: T): Int
-}
-
-
-
-class GeneralBSTImpl[T: TypeForBST](val value: T, val left: Option[GeneralBST[T]] = None, val right: Option[GeneralBST[T]] = None) extends GeneralBST[T] {
-  val comparator = implicitly[TypeForBST[T]]
+class GeneralBSTImpl[T](val value: T, val left: Option[GeneralBST[T]] = None, val right: Option[GeneralBST[T]] = None)(implicit ord: Ordering[T])  extends GeneralBST[T] {
 
   override def find(valueToFind: T) : Option[GeneralBST[T]] = {
-    if (comparator.compare(valueToFind, value) < 0) this.left.map(_.find(valueToFind)).getOrElse(None)
-    else if (comparator.compare(valueToFind, value) > 0) this.right.map(_.find(valueToFind)).getOrElse(None)
+    if (ord.compare(valueToFind, value) < 0) this.left.map(_.find(valueToFind)).getOrElse(None)
+    else if (ord.compare(valueToFind, value) > 0) this.right.map(_.find(valueToFind)).getOrElse(None)
     else Some(this)
   }
 
   override def add(newValue: T): GeneralBST[T] = {
-    if (comparator.compare(newValue, value) < 0) new GeneralBSTImpl(value, left, Some(right.map(_.add(newValue)).getOrElse(new GeneralBSTImpl[T](newValue))))
-      else if (comparator.compare(newValue, value) > 0) new GeneralBSTImpl(value, Some(left.map(_.add(newValue)).getOrElse(new GeneralBSTImpl[T](newValue))), right)
+    if (ord.compare(newValue, value) < 0) new GeneralBSTImpl(value, left, Some(right.map(_.add(newValue)).getOrElse(new GeneralBSTImpl[T](newValue))))
+      else if (ord.compare(newValue, value) > 0) new GeneralBSTImpl(value, Some(left.map(_.add(newValue)).getOrElse(new GeneralBSTImpl[T](newValue))), right)
         else this
   }
 }
 
-object ImplicitsDeclaration extends App{
-  implicit val float2BST: TypeForBST[Float] = new TypeForBST[Float] {
-    def compare(f: Float, s: Float) = f compare s
-  }
-
-  implicit val string2BST: TypeForBST[String] = new TypeForBST[String] {
-    def compare(f: String, s: String) = f compare s
-  }
-
-  implicit val watches2BST: TypeForBST[Watches] = new TypeForBST[Watches] {
+object ImplicitsDeclaration{
+  implicit val watches2BST: Ordering[Watches] = new Ordering[Watches] {
     def compare(f: Watches, s: Watches) = f.cost compare s.cost
   }
 }
